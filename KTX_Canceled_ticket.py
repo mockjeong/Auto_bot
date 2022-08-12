f3ceb0138833ca39c265bf82b97680abf7ce9f26
JMYoo <jeongmock@gmail.com>
2022-08-12 17:14:45 +0900

    KTX 취소표 예매 Update (22.08.12)
     - 멤버십 번호/비밀번호 입력 변경
     - 서울/강릉 프리셋 제거
     - 예매 시작 메시지 보완
     - 셀레니움 4에 맞게 수정 (fine_element 함수)
     - Telegram 예매 성공 메시지 보완

--- KTX_Canceled_ticket.py
+++ KTX_Canceled_ticket.py
@@ -13,9 +13,9 @@ import telegram
 telgm_token = '5523286949:AAH-dLh7miR9gU9aMrGUZHOOLiuRlWHm0q4'
 tele_bot = telegram.Bot(token = telgm_token)
 
-# ##코레일 ID 정보 입력
-# member_num = input('코레일멤버쉽 번호 입력 : ')
-# password = input('비밀 번호 입력 : ')
+##코레일 ID 정보 입력 (Membership 정보 입력 받을 경우)
+member_num = input('코레일멤버쉽 번호 입력 : ')
+password = input('비밀 번호 입력 : ')
 
 # ## 예매 정보 입력1
 # start = input('출발지 입력 : ')
@@ -24,25 +24,22 @@ tele_bot = telegram.Bot(token = telgm_token)
 # month = input('월 입력 (1~12): ')
 # day = input('일 입력 (1~31): ')
 # hous = input('출발시간 입력 (0~23): ') #0~23 사이로 입력#
-print ('여정 프리셋 입력하세요 (입력하지 않을경우 수동입력) ')
-print ('[1:서울-강릉(8/6), 2:청량리-강릉(8/6), 3:강릉-서울(8/7), 4:강릉-청량리(8/7)]')
-travel_preset = input('여정 프리셋 : ')
+
+# 프리셋 사용할 경우
+# print ('여정 프리셋 입력하세요 (입력하지 않을경우 수동입력) ')
+# print ('[1:서울-강릉(8/6), 2:청량리-강릉(8/6), 3:강릉-서울(8/7), 4:강릉-청량리(8/7)]')
+# travel_preset = input('여정 프리셋 : ')
+
+# 프리셋 사용 안할 경우
+travel_preset = 0
 
 ## 내 아이디로 미리 설정
 ## 지향이 아이디
 # member_num = '1040132735'
 # password = 'wlgid!9286'
 # 정목 아이디
-member_num = '0960000414'
-password = 'dbwjdahr11!'
-
-## 예매 정보 입력 (수동)
-# start = '서울'
-# end = '강릉'
-# year = '2022'
-# month = '8'
-# day = '6'
-# hours = '10'
+# member_num = '0960000414'
+# password = 'dbwjdahr11!'
 
 if travel_preset == '1':
     start = '서울'
@@ -80,6 +77,7 @@ else :
     day = input('일 입력 (1~31): ')
     hours = input('출발시간 입력 (0~23): ') #0~23 사이로 입력#
 
+print("\n*****************************")
 print("출발지 : " + start)
 print("도착지 : " + end)
 print("일  시 : " + year +"년 " + month + "월 " + day + "일 " + hours + "시")
@@ -95,14 +93,14 @@ driver.get('http://www.letskorail.com/korail/com/login.do') #코레일 로그인
 time.sleep(0.5)
 
 #코레일 멤버쉽 번호 입력
-driver.find_element_by_name('txtMember').send_keys(member_num)
+driver.find_element("name",'txtMember').send_keys(member_num)
 time.sleep(0.5)
 
 #비밀번호 입력
-driver.find_element_by_name('txtPwd').send_keys(password)
+driver.find_element("name",'txtPwd').send_keys(password)
 
 #로그인 버튼 클릭
-driver.find_element_by_xpath("/html/body/div[1]/div[3]/div/div[1]/div[2]/div[1]/div[1]/form[1]/fieldset/div[1]/ul/li[3]/a/img").click()
+driver.find_element("xpath","/html/body/div[1]/div[3]/div/div[1]/div[2]/div[1]/div[1]/form[1]/fieldset/div[1]/ul/li[3]/a/img").click()
 driver.implicitly_wait(3)
 time.sleep(0.5)
 
@@ -121,18 +119,18 @@ driver.get('http://www.letskorail.com/ebizprd/EbizPrdTicketpr21100W_pr21110.do')
 driver.implicitly_wait(3)
 
 #예매 정보 입력
-driver.find_element_by_name('txtGoStart').clear()
-driver.find_element_by_name('txtGoStart').send_keys(start)
-driver.find_element_by_name('txtGoEnd').clear()
-driver.find_element_by_name('txtGoEnd').send_keys(end)
-driver.find_element_by_name('selGoYear').send_keys(year)
-driver.find_element_by_name('selGoMonth').send_keys(month)
-driver.find_element_by_name('selGoDay').send_keys(day)
-driver.find_element_by_name('selGoHour').send_keys(hours)
+driver.find_element("name",'txtGoStart').clear()
+driver.find_element("name",'txtGoStart').send_keys(start)
+driver.find_element("name",'txtGoEnd').clear()
+driver.find_element("name",'txtGoEnd').send_keys(end)
+driver.find_element("name",'selGoYear').send_keys(year)
+driver.find_element("name",'selGoMonth').send_keys(month)
+driver.find_element("name",'selGoDay').send_keys(day)
+driver.find_element("name",'selGoHour').send_keys(hours)
 
 # 승차권 예매 누르기 
 xpath6="/html/body/div[1]/div[3]/div/div[1]/div[2]/form/div/p/a/img"
-driver.find_element_by_xpath(xpath6).click()
+driver.find_element("xpath",xpath6).click()
 time.sleep(1)
 
 # < 일반실 예매 처리 (td[6] : 일반실, td[5]:특실)>
@@ -144,13 +142,13 @@ while progress==0:
     try:
         #예약하기 or 입석+좌석 등.. 하이퍼링크가 살아있는경우
         res_but = "/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/a[1]/img"
-        button_type = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/a[1]/img").get_attribute('alt')
+        button_type = driver.find_element("xpath","/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/a[1]/img").get_attribute('alt')
         #다를경우 하이퍼링크 포함... 그러나 하이퍼링크가 좌석선택과 예약하기이경우 a[1]로 첫번째거 선택하도록 변경해야함...
     except:
         #페이지가 늦게 뜨는 경우에 대한 Error를 대비하기위해 try문 사용.. 페이지가 늦게 뜰경우 button 좌표 지정하지 않고 새로고침하도록 처리
         try:
             res_but = "/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/img"
-            button_type = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/img").get_attribute('alt')
+            button_type = driver.find_element("xpath","/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/img").get_attribute('alt')
             #매진시 하이퍼링크(a) 없이 다순 img로 표시됨
         except:
             button_type = ''
@@ -158,9 +156,9 @@ while progress==0:
 
     if button_type == "예약하기":
         try:
-            driver.find_element_by_xpath(res_but).click()
+            driver.find_element("xpath",res_but).click()
             driver.implicitly_wait(3)
-            tele_bot.sendMessage(chat_id = '5559539231', text="KTX 예약화면을 확인하세요..!!")
+            tele_bot.sendMessage(chat_id = '5559539231', text=start+"에서 "+end+"로 " + year +"년 " + month + "월 " + day + "일 " + hours + "시 여정이 예매되었습니다.\n"+"KTX 예약화면을 확인하세요..!!")
             progress = 1
             print("예매를 성공했습니다.")
         except:
@@ -168,7 +166,10 @@ while progress==0:
         break
     else:
         try:
-            driver.find_element_by_xpath(refresh_but).click()
+            driver.find_element("xpath",refresh_but).click()
         except:
             pass
     time.sleep(uniform(1.0,2.0))
+
+while(True):
+    pass
