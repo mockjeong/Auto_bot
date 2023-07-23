4713cfc31798815ea1223f64b7a1e78574ed3ddd
JMYoo <jeongmock@gmail.com>
2023-07-23 17:06:16 +0900

    7/23 울산 반영

--- KTX_Canceled_ticket.py
+++ KTX_Canceled_ticket.py
@@ -5,16 +5,22 @@ warnings.filterwarnings(action='ignore')
 
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
 import time
-import requests
-import bs4
-import random
 from bs4 import BeautifulSoup
 from random import *
 
 import telegram
-telgm_token = '5523286949:AAH-dLh7miR9gU9aMrGUZHOOLiuRlWHm0q4'
+telgm_token = '6093023324:AAHIMNZUiYqY9XYNwVIA5VUBTY7OqflC7N8'
 tele_bot = telegram.Bot(token = telgm_token)
+
+import asyncio
+
+async def send_Message(): #실행시킬 함수명 임의지정
+    bot = telegram.Bot(token = telgm_token)
+    await bot.send_message(5559539231,'KTX 예약화면을 확인하세요..!!')
+                # tele_bot.sendMessage(chat_id = '5559539231', text=start+"에서 "+end+"로 " + year +"년 " + month + "월 " + day + "일 " + hours + "시 여정이 예매되었습니다.\n"+"KTX 예약화면을 확인하세요..!!")
 
 ##코레일 ID 정보 입력 (Membership 정보 입력 받을 경우)
 # member_num = input('코레일멤버쉽 번호 입력 : ')
@@ -41,16 +47,19 @@ travel_preset = '1'
 # member_num = '1040132735'
 # password = 'wlgid!9286'
 # 정목 아이디
-member_num = '0960000414'
-password = 'dbwjdahr11!'
+# member_num = '0960000414'
+# password = 'dbwjdahr11!'
+# 아빠 아이디
+member_num = '0740248171'
+password = 'ymj1212!'
 
 if travel_preset == '1':
-    start = '서울'
-    end = '양평'
+    start = '광명'
+    end = '울산'
     year = '2023'
-    month = '2'
-    day = '3'
-    hours = '16'
+    month = '7'
+    day = '24'
+    hours = '11'
 elif travel_preset == '2':
     start = '청량리'
     end = '강릉'
@@ -130,11 +139,12 @@ driver.find_element("name",'selGoYear').send_keys(year)
 driver.find_element("name",'selGoMonth').send_keys(month)
 driver.find_element("name",'selGoDay').send_keys(day)
 driver.find_element("name",'selGoHour').send_keys(hours)
+time.sleep(1)
 
 # 승차권 예매 누르기 
 xpath6="/html/body/div[1]/div[3]/div/div[1]/div[2]/form/div/p/a/img"
 driver.find_element("xpath",xpath6).click()
-time.sleep(1)
+time.sleep(5)
 
 # < 일반실 예매 처리 (td[6] : 일반실, td[5]:특실)>
 progress = 0        ##초기 진입 : 0, 예약하기 완료시 : 1
@@ -143,25 +153,44 @@ refresh_but="/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[3]/p/a/img" ## 
 while progress==0:
     driver.implicitly_wait(1)
     try:
+        alert = Alert(driver)
+        # Accept the alert box
+        alert.accept()
+    except Exception as e:
+        print(e)
+    try:
         #예약하기 or 입석+좌석 등.. 하이퍼링크가 살아있는경우
-        res_but = "/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/a[1]/img"
-        button_type = driver.find_element("xpath","/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/a[1]/img").get_attribute('alt')
+        # res_but = "/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[5]/a[1]/img"  #특실
+        res_but = "//table/tbody/tr[1]/td[5]/a[1]/img"
+        button_type = driver.find_element(By.XPATH, res_but).get_attribute('alt')
+        # res_but = "/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/a[1]/img"
+        # button_type = driver.find_element("xpath",res_but).get_attribute('alt')
         #다를경우 하이퍼링크 포함... 그러나 하이퍼링크가 좌석선택과 예약하기이경우 a[1]로 첫번째거 선택하도록 변경해야함...
+    # except Exception as e:
+    #     print(e)
     except:
         #페이지가 늦게 뜨는 경우에 대한 Error를 대비하기위해 try문 사용.. 페이지가 늦게 뜰경우 button 좌표 지정하지 않고 새로고침하도록 처리
         try:
-            res_but = "/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/img"
-            button_type = driver.find_element("xpath","/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/img").get_attribute('alt')
+            # res_but = "/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[5]/img"   #특실
+            # # res_but = "/html/body/div[1]/div[3]/div/div[1]/form[1]/div/div[4]/table[1]/tbody/tr[1]/td[6]/img"
+            # button_type = driver.find_element("xpath",res_but).get_attribute('alt')
+            res_but = "//table/tbody/tr[1]/td[5]/img"
+            button_type = driver.find_element(By.XPATH, res_but).get_attribute('alt')
             #매진시 하이퍼링크(a) 없이 다순 img로 표시됨
         except:
-            button_type = ''
+            button_type = '오류'
             pass
-
+    print(button_type)
     if button_type == "예약하기":
         try:
             driver.find_element("xpath",res_but).click()
-            time.sleep(10)
-            tele_bot.sendMessage(chat_id = '5559539231', text=start+"에서 "+end+"로 " + year +"년 " + month + "월 " + day + "일 " + hours + "시 여정이 예매되었습니다.\n"+"KTX 예약화면을 확인하세요..!!")
+            time.sleep(2)
+            # Switch to the alert box
+            alert = Alert(driver)
+            # Accept the alert box
+            alert.accept()
+            # tele_bot.sendMessage(chat_id = '5559539231', text=start+"에서 "+end+"로 " + year +"년 " + month + "월 " + day + "일 " + hours + "시 여정이 예매되었습니다.\n"+"KTX 예약화면을 확인하세요..!!")
+            asyncio.run(send_Message()) #봇 실행하는 코드
             progress = 1
             print("예매를 성공했습니다.")
         except:
